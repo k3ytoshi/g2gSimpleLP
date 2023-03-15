7a3b56d316cc68bd238eb0fb700e241782091c05
ExeLxrd <tech.mccune@gmail.com>
2023-03-14 23:44:36 -0600

    Updates

---
+++ precache-manifest.2bd344b4ded0b1dc25505f68ca811b09.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2a1b88b7392505f05d6c82c645d214",
+    "url": "/LP/index.html"
+  },
+  {
+    "revision": "f1a46f4c07462cb0f10f",
+    "url": "/LP/static/css/main.4b8da867.chunk.css"
+  },
+  {
+    "revision": "18ce870e9cd3618d4cb8",
+    "url": "/LP/static/js/2.7a42462d.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/LP/static/js/2.7a42462d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a46f4c07462cb0f10f",
+    "url": "/LP/static/js/main.9141c5af.chunk.js"
+  },
+  {
+    "revision": "c5a57e32672680c4def3",
+    "url": "/LP/static/js/runtime-main.4eeb739a.js"
+  },
+  {
+    "revision": "b732bf72292bbccac0d93807811b4864",
+    "url": "/LP/static/media/download__14_-removebg-preview.b732bf72.svg"
+  },
+  {
+    "revision": "06d64c2278ccf9a440645272dedaba7b",
+    "url": "/LP/static/media/mail.06d64c22.svg"
+  },
+  {
+    "revision": "cf89919fb4529896970ebf172ff0672a",
+    "url": "/LP/static/media/newsletter.cf89919f.svg"
+  },
+  {
+    "revision": "71f30eae279c44ee3362b43ef27f4c04",
+    "url": "/LP/static/media/privacy.71f30eae.md"
+  }
+]);
